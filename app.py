0c9a04540ca9a6723120c097cce5285dedcd2f16
fiansyaaa <yrafiansya@gmail.com>
2025-06-04 14:44:30 +0700

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-app.py
-
 import streamlit as st
 
 st.set_page_config(layout="centered", page_title="Anemia Prediction")
 
-st.title("Prediksi Anemia") st.header("Selamat datang di Aplikasi Prediksi Anemia")
+st.title("Prediksi Anemia")
+st.header("Selamat datang di Aplikasi Prediksi Anemia")
 
 st.markdown("""
+### Fitur Aplikasi:
+- Menampilkan data pasien
+- Prediksi kemungkinan anemia berdasarkan fitur input
+- Visualisasi interaktif dan hasil klasifikasi
 
-Fitur Aplikasi:
-
-Menampilkan data pasien
-
-Prediksi kemungkinan anemia berdasarkan fitur input
-
-Visualisasi interaktif dan hasil klasifikasi
-
-
-Silakan gunakan menu di sebelah kiri untuk menjelajahi slide. """)
+Silakan gunakan menu di sebelah kiri untuk menjelajahi slide.
+""")
