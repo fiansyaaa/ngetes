8609bc1749f230b2a7d78bf3ba90b885b62556f6
fiansyaaa <yrafiansya@gmail.com>
2025-06-04 14:41:08 +0700

    1_data.py

--- pages/1_data.py
+++ pages/1_data.py
@@ -1,17 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-st.title("📊 Data Alpukat")
-
-df = pd.read_csv("data/avocado_ripeness_dataset.csv")
+st.title("Data Pasien")
+df = pd.read_csv("anemia_dataset.csv")
 st.dataframe(df)
-
-# Visualisasi distribusi berat
-st.subheader("Histogram Berat")
-if "Berat" in df.columns:
-    fig, ax = plt.subplots()
-    df["Berat"].hist(bins=20, color="green", edgecolor="black", ax=ax)
-    st.pyplot(fig)
-else:
-    st.error("Kolom 'Berat' tidak ditemukan dalam dataset.")
